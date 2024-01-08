c0f7dd12dc30210ce0c179eb294962b761930562
jumaxbenz <108999680+jumaxbenz@users.noreply.github.com>
2024-01-08 23:24:06 +0100

    Update App.tsx

--- unitedconflicts/App.tsx
+++ unitedconflicts/App.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import { withAuthenticator } from '@aws-amplify/ui-react';
-import Amplify from 'aws-amplify';
-import awsExports from './aws-exports'; // Stellen Sie sicher, dass der Pfad korrekt ist
-
-Amplify.configure(awsExports);
+import Amplify from 'aws-amplify'
 
 function App() {
   return (
